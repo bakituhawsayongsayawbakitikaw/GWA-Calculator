5997af1aa4f99f06c5ab07161392345501c1f1c5
bakituhawsayongsayawbakitikaw <gabrielsantoc05@gmail.com>
2024-01-06 14:44:40 +0800

    add compute function for total number of units

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const btnAddSub = document.querySelector('.add-but')
 
-btnAddSub.addEventListener("click", addNewSub);
+const btnAddSub = document.querySelector('.add-but')
+btnAddSub.addEventListener("click", addNewSub)
 
 function addNewSub() {
     const newForm = document.createElement('form')
@@ -10,7 +10,7 @@ function addNewSub() {
         <div class="input" >
             <div class="input-container">
                 <input type="text" name="subject" class="fields">
-                <input type="number" min="1" max="5" name="unit" class="fields">
+                <input type="number" min="1" max="5" name="unit" class="fields unit-value">
                 <input type="number" name="grade" class="fields">
                 <button class="del-but">X</button>
             </div>
@@ -31,13 +31,28 @@ function removeSub( event ) {
     }
 }
 
-// document.addEventListener('click', function (event) {
-//     // Check if the clicked element is a delete button with the class 'del-but'
-//     if (event.target.classList.contains('del-but')) {
-//         // Get the parent form element and remove it
-//         const formToRemove = event.target.closest('.input-row');
-//         if (formToRemove) {
-//             formToRemove.remove();
-//         }
-//     }
-// });
+function reset() {
+
+}
+
+const btnCompute = document.querySelector('.compute-btn')
+btnCompute.addEventListener("click", compute)
+
+function compute() {
+    const units = document.getElementsByClassName('unit-value')
+
+    const listOfUnits = []
+    let totalUnits = 0
+
+    for(let i = 0; i < units.length; i++) {
+        listOfUnits.push(parseFloat(units[i].value))
+    }
+
+    listOfUnits.forEach((e) => {
+        totalUnits += e
+    });
+
+    let totalUnitHTML = document.getElementById('units-total')
+    totalUnitHTML.innerHTML = `Total Number of Units: ${totalUnits}`
+    
+}
